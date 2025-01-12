590eb02ffda639646451ebfeb9a3922748565db9
Rhobtor <luismidp7@gmail.com>
2025-01-12 19:25:55 +0100

    final model and upload simulink model

--- workspace_test/build/car/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/mnt/c/Users/luism/Documents/Ubuntu/reactive_navegation/workspace_test/install/car'
--- workspace_test/build/car_cpp/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/luism/Documents/Ubuntu/reactive_navegation/workspace_test/src/car_cpp
-# Build directory: /mnt/c/Users/luism/Documents/Ubuntu/reactive_navegation/workspace_test/build/car_cpp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
