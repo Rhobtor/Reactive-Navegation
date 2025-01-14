67ddfe62aac13cafb871947219e7260efc138a58
rhobtor <luismidp7@gmail.com>
2025-01-14 10:11:38 +0100

    model only lidar

--- workspace/build/car/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/mnt/c/Users/luism/Documents/Ubuntu/Reactive-Navegation/Reactive-Navegation/workspace/install/car'
